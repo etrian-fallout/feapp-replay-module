130414f36a0bf543e7462e9d10175e1b22ad5794
PayDevD <mrpaydfordev@gmail.com>
2019-07-27 17:21:57 +0900

    remove select tag

--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import { Route } from "react-router-dom";
+import App from "./App";
 import Lol from "./components/Lol";
 import Pubg from "./components/Pubg";
 import About from "./components/About";
 
 const Root = () => {
   return (
-    <BrowserRouter><switch>
-    <Route exact path="/" component={App} />
-    <Route path="/about" component={About} />
-    <Route path="/lol/" component={Lol} />
-    <Route path="/pubg" component={Pubg} />
-  </switch>
+    <BrowserRouter>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/lol/" component={Lol} />
+        <Route path="/pubg" component={Pubg} />
     </BrowserRouter>
   );
 };
