b89c771b918aef798f44042ba83f49af82c71e44
yongjun823 <diddydwns135@gmail.com>
2019-07-30 14:06:59 +0900

    replay page background map image

--- src/components/Pubg/Replay.js
+++ src/components/Pubg/Replay.js
@@ -1,17 +1,62 @@
 import React from "react";
+import { Stage, Sprite, Graphics } from "@inlet/react-pixi"
 
 class ReplayPubg extends React.Component {
   state = {
-    matchId: ""
+    matchId: "",
+    mapName: "",
+    telemetryUrl: ""
   };
 
-  componentDidMount() {
-    this.setState({
-      matchId: this.props.match.params.matchId
+  drawCircle = () => (
+    <Graphics
+      draw={
+        g => {
+          g.lineStyle(0)
+          g.beginFill(0xffff0b, 0.5)
+          g.drawCircle(470, 90, 60)
+          g.endFill()
+        }
+      }
+    />
+  )
+
+  async componentDidMount() {
+    await this.setState({
+      matchId: this.props.match.params.matchId,
+      mapName: this.props.location.state.mapName,
+      telemetryUrl: this.props.location.state.telemetryUrl
     });
+
+    console.log(this.state);
+
+    const data = await fetch(
+      this.state.telemetryUrl, {
+        // "http://localhost:3000/mm.json", {
+        method: 'get',
+        headers: new Headers({
+          "Accept": "application/vnd.api+json",
+          "Accept-Encoding": "gzip"
+        })
+      }).then(res => res.json())
+
+    console.log(data)
   }
+
   render() {
-    return <div>{this.state.matchId}</div>;
+    return <div>
+      <div>{this.state.matchId}</div>
+      <Stage width={1000} height={1000} options={{
+        backgroundColor: 0x1099bb,
+        antialias: true,
+        transparent: false,
+        resolution: 1
+      }}>
+
+        <Sprite image={`http://localhost:3000/asset/${this.state.mapName}_Main_Low_Res.png`} x={0} y={0} />
+
+      </Stage>
+    </div>;
   }
 }
 
