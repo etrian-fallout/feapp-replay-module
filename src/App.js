77d0c6fe5a566add0b559db1ae34e87b903546fb
PayDevD <45145580+PayDevD@users.noreply.github.com>
2019-07-31 22:13:58 +0900

    remove del tag (#10)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>PUBG <del>/ LOL</del> Minimap Replay</h1>
+        <h1>PUBG Map Replay</h1>
       </header>
       <Navigation />
       <GameNavigation />
